d86861db86e90bee417e998a1f52b90e2a92e281
martinbrumana <martinbrumana@gmail.com>
2023-02-03 20:36:28 -0300

    markup

---
+++ js/func.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper-logos", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
