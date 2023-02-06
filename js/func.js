dd2b984e6102b799879d2a4ffddd930b3fa21360
martinbrumana <martinbrumana@gmail.com>
2023-02-05 23:04:45 -0300

    desktop mobile

--- js/func.js
+++ js/func.js
@@ -15,7 +15,30 @@ $(document).ready(function () {
     $(".time-toggle li").removeClass("active");
     $(this).addClass("active");
     let mensualOrAnual = $(this).html();
-    console.log(mensualOrAnual);
+
+    if (mensualOrAnual == "Mensual") {
+      $(".tb-1 .tf-footer span").html("$0 / Mes");
+
+      $(".tb-2 .tf-footer span").html("$59.900 / Mes");
+      $(".tb-2 .tf-footer label").html("$6.900 / Persona");
+
+      $(".tb-3 .tf-footer span").html("$59.900 / Mes");
+      $(".tb-3 .tf-footer label").html("$11.900 / Mes / Persona");
+
+      $(".tb-4 .tf-footer span").html("$399.900 / Mes");
+      $(".tb-4 .tf-footer label").html("$19.900 / Persona");
+    } else {
+      $(".tb-1 .tf-footer span").html("$0 / Anual");
+
+      $(".tb-2 .tf-footer span").html("$718,800 / Anual");
+      $(".tb-2 .tf-footer label").html("$82.800 / Persona");
+
+      $(".tb-3 .tf-footer span").html("$718,800 / Anual");
+      $(".tb-3 .tf-footer label").html("$142,800 / Persona");
+
+      $(".tb-4 .tf-footer span").html("$4,798,800 / Anual");
+      $(".tb-4 .tf-footer label").html("$238,800 / Persona");
+    }
   });
 
   // ver planes completo
